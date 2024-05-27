ce71db23784784f2bfe08c7de76b97448178db75
Costantino14 <costantino.grabesu14@gmail.com>
2024-05-27 18:52:43 +0200

    aggiunta bottoni

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import './App.css';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import MyNav from "./Components/MyNav";
 import MyFooter from './Components/MyFooter';
 import Welcome from './Components/Welcome';
 import AllTheBooks from './Components/AllTheBooks';
-import fantasy from './Books/fantasy.json'
+import fantasy from './Books/fantasy.json';
+import history from './Books/history.json';
+import romance from './Books/romance.json';
+import horror from './Books/horror.json';
+import scifi from './Books/scifi.json';
+
 
 
 function App() {
+
+  let [type, setType] = useState('fantasy');
   return (
     <>
       <MyNav />
       <Container className="my-3">
       <Welcome />
-      <AllTheBooks books={fantasy}/>
+      <Button className='mx-2' variant='dark' onClick={() => setType('history')}>Storico</Button>
+      <Button className='mx-2' variant='dark' onClick={() => setType('fantasy')}>Fantasia</Button>
+      <Button className='mx-2' variant='dark' onClick={() => setType('horror')}>Horror</Button>
+      <Button className='mx-2' variant='dark' onClick={() => setType('romance')}>Romantico</Button>
+      <Button className='mx-2' variant='dark' onClick={() => setType('scifi')}>scifi</Button>
+      
+      {type === 'history' && <AllTheBooks books={history}/>}
+      {type === 'fantasy' && <AllTheBooks books={fantasy}/>}
+      {type === 'horror' && <AllTheBooks books={horror}/>}
+      {type === 'romance' && <AllTheBooks books={romance}/>}
+      {type === 'scifi' && <AllTheBooks books={scifi}/>}
       </Container>
       <MyFooter />
     </>
